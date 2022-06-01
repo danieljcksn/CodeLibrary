cb9289c4c1e52252e831e44d9c577571adec9afd
Daniel <daniiel.cavallcante@gmail.com>
2022-06-01 09:50:09 -0300

    Folders structure, bfs and cpp template

---
+++ Graph/BFS.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<vector<int>> graph;
+vector<bool> visited;
+vector<int> cities;
+
+void bfs(int root, graph& g){
+    visited.assign(g.size()+1, false);
+
+    queue<int> q;
+    q.push(root);
+    visited[root] = true;
+
+    while(!q.empty()){
+        int v = q.front();
+        q.pop();
+
+        for(int u:g[v]){
+            if(!visited[u]){
+                q.push(u);
+                visited[u] = true;
+            }
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, edges;
+    cin >> n >> edges;
+
+    //1-indexado
+    graph g(n+1);
+
+    for(int i=0, a, b; i<edges; ++i){
+        cin >> a >> b;
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }    
+
+    bfs(1, g);
+
+    return 0;
+}
---
+++ template.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define EPS 1e-9
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+#define MOD 1000000007
+#define PI 3.141592653589793238462643383279502884
+#define all(x) x.begin(), x.end()
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL); 
+#define mp make_pair
+#define pb push_back
+#define sz(x) int(x.size())
+#define endl '\n'
+using ll = long long int;
+using ull = unsigned long long int;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vll;
+
+int main(){
+    fastio
+    
+    
+
+    return 0;
+}
